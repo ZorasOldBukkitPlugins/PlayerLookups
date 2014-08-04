bfc4a23daf614d9301ae3920e1e4c1fdde05bbfd
Zora <MrZoraman@Gmail.com>
2014-08-04 05:58:09 -0700

    A little bit of formatting

--- src/com/lagopusempire/playerlookups/utils/MetadataUtils.java
+++ src/com/lagopusempire/playerlookups/utils/MetadataUtils.java
@@ -7,8 +7,13 @@ import org.bukkit.metadata.MetadataValue;
 import org.bukkit.metadata.Metadatable;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ *
+ * @author MrZoraman
+ */
 public class MetadataUtils
 {
+
     private final JavaPlugin plugin;
 
     public MetadataUtils(JavaPlugin plugin)
@@ -24,9 +29,9 @@ public class MetadataUtils
     public Object getMetadata(Metadatable object, String key)
     {
         List<MetadataValue> values = object.getMetadata(key);
-        for(MetadataValue value : values)
+        for (MetadataValue value : values)
         {
-            if(value.getOwningPlugin() == plugin)
+            if (value.getOwningPlugin() == plugin)
             {
                 return value.value();
             }
@@ -34,27 +39,27 @@ public class MetadataUtils
 
         return null;
     }
-    
+
     public void removeMetadata(Metadatable object, String key)
     {
         object.removeMetadata(key, plugin);
     }
-    
+
     public void setBoolean(Metadatable object, String key, boolean value)
     {
-        if(value)
+        if (value)
         {
             object.setMetadata(key, new FixedMetadataValue(plugin, new Object()));
         }
         else
         {
-            if(object.hasMetadata(key))
+            if (object.hasMetadata(key))
             {
                 object.removeMetadata(key, plugin);
             }
         }
     }
-    
+
     public boolean getBoolean(Metadatable object, String key)
     {
         return object.hasMetadata(key);
