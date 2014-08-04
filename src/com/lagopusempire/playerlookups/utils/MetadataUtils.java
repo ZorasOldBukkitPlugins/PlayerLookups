3d0c05f9016baffe6fd059dc835d1971f6c528f0
Zora <MrZoraman@Gmail.com>
2014-08-04 05:57:44 -0700

    Added MetadataUtils from moonphasereactor

---
+++ src/com/lagopusempire/playerlookups/utils/MetadataUtils.java
@@ -0,0 +1,62 @@
+package com.lagopusempire.playerlookups.utils;
+
+import java.util.List;
+
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.metadata.Metadatable;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MetadataUtils
+{
+    private final JavaPlugin plugin;
+
+    public MetadataUtils(JavaPlugin plugin)
+    {
+        this.plugin = plugin;
+    }
+
+    public void setMetadata(Metadatable object, String key, Object value)
+    {
+        object.setMetadata(key, new FixedMetadataValue(plugin, value));
+    }
+
+    public Object getMetadata(Metadatable object, String key)
+    {
+        List<MetadataValue> values = object.getMetadata(key);
+        for(MetadataValue value : values)
+        {
+            if(value.getOwningPlugin() == plugin)
+            {
+                return value.value();
+            }
+        }
+
+        return null;
+    }
+    
+    public void removeMetadata(Metadatable object, String key)
+    {
+        object.removeMetadata(key, plugin);
+    }
+    
+    public void setBoolean(Metadatable object, String key, boolean value)
+    {
+        if(value)
+        {
+            object.setMetadata(key, new FixedMetadataValue(plugin, new Object()));
+        }
+        else
+        {
+            if(object.hasMetadata(key))
+            {
+                object.removeMetadata(key, plugin);
+            }
+        }
+    }
+    
+    public boolean getBoolean(Metadatable object, String key)
+    {
+        return object.hasMetadata(key);
+    }
+}
