0ed5bede7e46e1f56db5381beb12f9b8d4d31aa2
Zora <MrZoraman@Gmail.com>
2014-08-05 06:15:36 -0700

    Documentation

--- src/com/lagopusempire/playerlookups/PlayerInfo.java
+++ src/com/lagopusempire/playerlookups/PlayerInfo.java
@@ -4,6 +4,10 @@ package com.lagopusempire.playerlookups;
 import java.util.Date;
 import java.util.UUID;
 
+/**
+ * Holds info retrieved by PlayerLookups. Not all fields will be initialized!
+ * @author MrZoraman
+ */
 public class PlayerInfo
 {
     public UUID uuid = null;
